5a0914f8b1dcd8efd321aa83c5e298a3a58315a2
Serena-Dong <serenadong3159@icloud.com>
2023-02-22 18:01:22 +0100

    hello world view option 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+
+    $text = 'Hello World';
+
+    return view('home', compact('text'));
 });
